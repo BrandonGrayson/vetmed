b34eb370b80a6d5f56a9223feedc5fa52c92d445
Brandon <brandonbgrayson@gmail.com>
2022-12-14 03:04:24 -0500

    sign up user form format

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import SignUpUser from "./components/SignUpUserForm";
 import { Grid } from "@mui/material";
 
 function App() {
-  return (
-    <Grid container>
-      <Grid>
-        <SignUpUser />
-      </Grid>
-    </Grid>
-  );
+  return <SignUpUser />;
 }
 
 export default App;
--- src/components/SignUpUserForm.tsx
+++ src/components/SignUpUserForm.tsx
@@ -1,10 +1,75 @@
-import { Box, TextField } from "@mui/material";
+import { Grid, TextField, Box, Button, Typography } from "@mui/material";
 
 export default function SignUpUser() {
   return (
-    <Box sx={{ border: "1px solid grey", width: "300px", maxWidth: "100%" }}>
-      <TextField id="outlined-basic" label="Email" variant="outlined" />
-      <TextField id="outlined-basic" label="Password" variant="outlined" />
-    </Box>
+    <Grid container>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "100px",
+          marginBottom: "20px",
+        }}
+      >
+        <Typography variant="h5">Sign Up</Typography>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          width: "100%",
+          maxWidth: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            border: "1px solid grey",
+            width: "300px",
+            display: "flex",
+            flexDirection: "column",
+            height: "300px",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            sx={{
+              marginBottom: "20px",
+              width: "90%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: "45px",
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            sx={{
+              marginBottom: "20px",
+              width: "90%",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          />
+          <Button
+            sx={{
+              marginTop: "20px",
+              width: "90%",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+            variant="contained"
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
